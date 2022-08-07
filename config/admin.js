79a9931420b471819b10b9b58fa05804a73b75a9
Alexey T <rostovboy@gmail.com>
2022-08-07 15:56:54 +0300

    Changed admin config

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'd9b0df66ff97a666027e665707b4e3e7'),
+  },
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '80ccc3aa5b4e2deb07e45f709ba6f2ec'),
+    secret: env('ADMIN_JWT_SECRET', '77b2c87dbab4e1697bec244226fbd1b3'),
   },
 });
