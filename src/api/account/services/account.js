f62662599d328df4efbe94f377a08807929a43ce
Alexey T <rostovboy@gmail.com>
2022-08-07 11:02:10 +0300

    Account Collection Service

--- src/api/account/services/account.js
+++ src/api/account/services/account.js
@@ -6,4 +6,17 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::account.account');
+module.exports = createCoreService('api::account.account', ({ strapi }) => ({
+    newUser(user_id) {
+        return strapi.service('api::account.account').create({ 
+        data: { 
+              balance: 0, user: user_id 
+            }
+        });
+    },
+    getUserAccount(user_id) {
+        return strapi.db.query('api::account.account').findOne({
+            where: { user: user_id },
+        })
+    }
+}));
